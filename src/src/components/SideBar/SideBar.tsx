f5346a5549cdd216369778fcfaac8f4bcdbd38d3
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2023-07-10 13:07:30 +0100

    Make npm start work with FML (#10)
    * Make npm start work with FML
    
    * Introduce getBaseHost helper

--- src/src/components/SideBar/SideBar.tsx
+++ src/src/components/SideBar/SideBar.tsx
@@ -10,14 +10,13 @@ import { IconName } from 'components/kit/Icon';
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 
 import { PathEnum } from 'config/enums/routesEnum';
-import { getAPIHost } from 'config/config';
+import { getBaseHost } from 'config/config';
 import { ANALYTICS_EVENT_KEYS } from 'config/analytics/analyticsKeysMap';
 import { DOCUMENTATIONS } from 'config/references';
 
 import routes, { IRoute } from 'routes/routes';
 
 import { trackEvent } from 'services/analytics';
-import NetworkService from 'services/NetworkService';
 
 import { getItem } from 'utils/storage';
 
@@ -27,7 +26,7 @@ function SideBar(): React.FunctionComponentElement<React.ReactNode> {
   const [version, setVersion] = React.useState('unknown');
 
   useEffect(() => {
-    fetch('/version').then((response) => {
+    fetch(`${getBaseHost()}/version`).then((response) => {
       response.text().then((version) => {
         setVersion(version);
       });
