3ebdbcbe38bbdb56ea49abbce7bb3233b9cf9665
Gbolahan Fakorede <gbolahanf.14@gmail.com>
2023-07-27 12:19:35 +0100

    Add code sample to integrate fastai

--- src/src/pages/Dashboard/components/AimIntegrations/AimIntegrations.tsx
+++ src/src/pages/Dashboard/components/AimIntegrations/AimIntegrations.tsx
@@ -114,23 +114,20 @@ model.fit(train_data, train_labels, log_cout=AimLogger(loss_function='Logloss'),
       title: 'Integrate fastai',
       docsLink: DOCUMENTATIONS.INTEGRATIONS.FASTAI,
       code: `import mlflow.fastai
-import mlflow
 
 # Set FastTrackML tracking server
-mlflow.set_tracking_uri("${fasttrack_server}")
+mlflow.set_tracking_uri(${fasttrack_server})
 
-# Define the Learner model
-model = ...
+# ...
 
-# log the fastai Leaner model
+# Enable auto logging
+mlflow.fastai.autolog()
+
+# Start MLflow session
 with mlflow.start_run() as run:
     model.fit(epochs, learning_rate)
-    mlflow.fastai.log_model(model, "model")
 
-# Load the model for scoring
-model_uri = "runs:/{}/model".format(run.info.run_id)
-loaded_model = mlflow.fastai.load_model(model_uri)
-results = loaded_model.predict(predict_data)`,
+# ...`,
     },
     {
       title: 'Integrate LightGBM',
