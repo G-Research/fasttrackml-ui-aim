23be8f56aa85be68c6cc86812f2229239c8d0bcc
Juan Escalada <juanescalada175@gmail.com>
2024-01-26 11:24:09 -0800

    Fix onParamsScaleTypeChange logic to handle selectedParams state

--- src/src/utils/app/onParamsScaleTypeChange.ts
+++ src/src/utils/app/onParamsScaleTypeChange.ts
@@ -2,9 +2,11 @@ import { ANALYTICS_EVENT_KEYS } from 'config/analytics/analyticsKeysMap';
 
 import * as analytics from 'services/analytics';
 
-import { IParamsScaleStates } from 'types/components/ParamsScalePopover/ParamsScalePopover';
 import { IModel, State } from 'types/services/models/model';
-import { IAppModelConfig } from 'types/services/models/explorer/createAppModel';
+import {
+  IAppModelConfig,
+  ISelectOption,
+} from 'types/services/models/explorer/createAppModel';
 
 export default function onParamsScaleTypeChange<M extends State>({
   args,
@@ -12,7 +14,7 @@ export default function onParamsScaleTypeChange<M extends State>({
   appName,
   updateModelData,
 }: {
-  args: IParamsScaleStates;
+  args: ISelectOption[];
   model: IModel<M>;
   appName: string;
   updateModelData: (
@@ -24,20 +26,17 @@ export default function onParamsScaleTypeChange<M extends State>({
   if (configData?.chart) {
     configData = {
       ...configData,
-      chart: {
-        ...configData.chart,
-        paramsScaleType: args,
+      select: {
+        ...configData.select,
+        options: args,
       },
     };
     model.setState({ config: configData });
     updateModelData(configData, true);
   }
+  // todo create analytics keys and change this
   analytics.trackEvent(
     // @ts-ignore
     `${ANALYTICS_EVENT_KEYS[appName].chart.controls.changeParamsScale} to "${args.xAxis}"`,
-  );
-  analytics.trackEvent(
-    // @ts-ignore
-    `${ANALYTICS_EVENT_KEYS[appName].chart.controls.changeParamsScale} to "${args.yAxis}"`,
   );
 }
