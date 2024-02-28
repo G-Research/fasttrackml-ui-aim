04fd838826bbf3023f15111b3619d3e891fa36e1
Juan Escalada <juanescalada175@gmail.com>
2024-02-27 22:23:15 -0800

    Update selectedExperiments toggle function

--- src/src/utils/app/onSelectExperimentNamesChange.ts
+++ src/src/utils/app/onSelectExperimentNamesChange.ts
@@ -1,5 +1,9 @@
 import { IModel, State } from 'types/services/models/model';
 
+import { setItem } from 'utils/storage';
+
+import { getSelectedExperimentNames } from './getSelectedExperimentNames';
+
 export default function onSelectExperimentNamesChange<M extends State>({
   experimentName,
   model,
@@ -8,8 +12,7 @@ export default function onSelectExperimentNamesChange<M extends State>({
   model: IModel<M>;
 }) {
   const configData = model.getState()?.config;
-  const selectedExperimentNames =
-    configData?.select?.selectedExperimentNames || [];
+  const selectedExperimentNames = getSelectedExperimentNames();
 
   // If the experiment is already selected, remove it from the list
   // Otherwise, add it
@@ -19,6 +22,12 @@ export default function onSelectExperimentNamesChange<M extends State>({
     selectedExperimentNames.splice(index, 1);
   } else {
     selectedExperimentNames.push(experimentName);
+  }
+
+  if (selectedExperimentNames.length === 0) {
+    setItem('selectedExperimentNames', '');
+  } else {
+    setItem('selectedExperimentNames', selectedExperimentNames);
   }
 
   if (configData?.select) {
