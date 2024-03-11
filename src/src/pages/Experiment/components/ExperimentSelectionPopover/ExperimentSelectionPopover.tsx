6fad87f5b1947ee181ea0fa7ebf5a45fa4242be6
Juan Escalada <juanescalada175@gmail.com>
2024-03-11 18:47:06 +0900

    Add regex filtering to experiment header

--- src/src/pages/Experiment/components/ExperimentSelectionPopover/ExperimentSelectionPopover.tsx
+++ src/src/pages/Experiment/components/ExperimentSelectionPopover/ExperimentSelectionPopover.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import classNames from 'classnames';
 import moment from 'moment';
 
-import { Button } from '@material-ui/core';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import { Button, InputBase, Tooltip } from '@material-ui/core';
 
 import ErrorBoundary from 'components/ErrorBoundary/ErrorBoundary';
 import { Icon, Spinner, Text } from 'components/kit';
 
 import { DATE_WITH_SECONDS } from 'config/dates/dates';
+
+import { IExperimentData } from 'modules/core/api/experimentsApi';
 
 import { IExperimentSelectionPopoverProps } from '.';
 
@@ -19,6 +22,7 @@ function ExperimentSelectionPopover({
   isExperimentsLoading,
   getExperimentsData,
   onSelectExperimentNamesChange,
+  onToggleAllExperiments,
 }: IExperimentSelectionPopoverProps): React.FunctionComponentElement<React.ReactNode> {
   React.useEffect(() => {
     if (!experimentsData) {
@@ -30,6 +34,17 @@ function ExperimentSelectionPopover({
   // Add forceUpdate to force re-render (to update selected experiments)
   const [, updateState] = React.useState<{}>();
   const forceUpdate = React.useCallback(() => updateState({}), []);
+
+  const [searchValue, setSearchValue] = React.useState<string>('');
+  const [isRegexSearch, setIsRegexSearch] = React.useState(false);
+  const [invalidRegex, setInvalidRegex] = React.useState<boolean>(false);
+  const [visibleExperiments, setVisibleExperiments] = React.useState<
+    IExperimentData[]
+  >([]);
+
+  React.useEffect(() => {
+    setVisibleExperiments(experimentsData || []);
+  }, [experimentsData]);
 
   // TODO: Add shortening in the middle rather than at the end
   function shortenExperimentName(name?: string): string {
@@ -54,6 +69,41 @@ function ExperimentSelectionPopover({
     return selectedExperimentNames.includes(experimentName);
   }
 
+  function handleSearchInputChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+  ): void {
+    setSearchValue(e.target.value);
+
+    if (isRegexSearch) {
+      try {
+        const regex = new RegExp(e.target.value, 'i');
+        setInvalidRegex(false);
+        const options = experimentsData?.filter((experiment) =>
+          regex.test(experiment.name),
+        );
+        setVisibleExperiments(options || []);
+      } catch (error) {
+        setInvalidRegex(true);
+      }
+    } else {
+      const options = experimentsData?.filter((experiment) =>
+        experiment.name.toLowerCase().includes(e.target.value.toLowerCase()),
+      );
+      setVisibleExperiments(options || []);
+    }
+  }
+
+  function selectAllExperiments() {
+    const allExperimentNames = experimentsData?.map(
+      (experiment) => experiment.name,
+    );
+    onToggleAllExperiments(allExperimentNames || []);
+  }
+
+  function removeAllExperiments() {
+    onToggleAllExperiments([]);
+  }
+
   return (
     <ErrorBoundary>
       <div className='ExperimentSelectionPopover'>
@@ -62,13 +112,59 @@ function ExperimentSelectionPopover({
             <Text size={14} tint={100} weight={700}>
               Experiments
             </Text>
+            <div className='ExperimentSelectionPopover__headerContainer__buttons'>
+              <Button
+                onClick={selectAllExperiments}
+                className='ExperimentSelectionPopover__headerContainer__selectAllButton'
+              >
+                <Text size={12} tint={5} weight={500}>
+                  Select All
+                </Text>
+              </Button>
+              <Button
+                onClick={removeAllExperiments}
+                className='ExperimentSelectionPopover__headerContainer__removeAllButton'
+                size='small'
+              >
+                <Text size={12} tint={5} weight={500}>
+                  Remove All
+                </Text>
+              </Button>
+            </div>
           </div>
         </div>
         <div className='ExperimentSelectionPopover__contentContainer'>
           <div className='ExperimentSelectionPopover__contentContainer__experimentsListContainer'>
             <div className='ExperimentSelectionPopover__contentContainer__experimentsListContainer__experimentList ScrollBar__hidden'>
+              <div
+                className={
+                  invalidRegex
+                    ? 'ExperimentSelectionPopover__searchContainer error'
+                    : 'ExperimentSelectionPopover__searchContainer'
+                }
+              >
+                <InputBase
+                  placeholder='Search'
+                  value={searchValue}
+                  onChange={handleSearchInputChange}
+                  inputProps={{ 'aria-label': 'search' }}
+                  className='ExperimentSelectionPopover__searchContainer__inputBase'
+                />
+                <Tooltip title='Use Regular Expression'>
+                  <ToggleButton
+                    value='check'
+                    selected={isRegexSearch}
+                    onChange={() => {
+                      setIsRegexSearch(!isRegexSearch);
+                    }}
+                    className='RegexToggle'
+                  >
+                    .*
+                  </ToggleButton>
+                </Tooltip>
+              </div>
               {!isExperimentsLoading ? (
-                experimentsData?.map((experiment) => (
+                visibleExperiments?.map((experiment) => (
                   <Button
                     key={experiment.id}
                     onClick={() => handleExperimentClick(experiment.name)}
