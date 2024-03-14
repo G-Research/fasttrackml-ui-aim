7b10b60236057ff9e0c55a2b69ed60da24227244
Juan Escalada <juanescalada175@gmail.com>
2024-03-14 16:09:33 +0900

    Update onToggleAllExperiments

--- src/src/utils/app/onToggleAllExperiments.ts
+++ src/src/utils/app/onToggleAllExperiments.ts
@@ -1,6 +1,6 @@
 import { IModel, State } from 'types/services/models/model';
 
-import { setItem } from 'utils/storage';
+import onSelectExperimentNamesChange from './onSelectExperimentNamesChange';
 
 export default function onToggleAllExperiments<M extends State>({
   experimentNames,
@@ -9,19 +9,8 @@ export default function onToggleAllExperiments<M extends State>({
   experimentNames: string[];
   model: IModel<M>;
 }) {
-  const configData = model.getState()?.config;
-
-  if (experimentNames.length === 0) {
-    setItem('selectedExperimentNames', '');
-  } else {
-    setItem('selectedExperimentNames', experimentNames);
-  }
-
-  if (configData?.select) {
-    const newConfig = {
-      ...configData,
-      select: { ...configData.select, experimentNames },
-    };
-    model.setState({ config: newConfig });
-  }
+  // Toggle all experiments in list one by one
+  experimentNames.forEach((experimentName) => {
+    onSelectExperimentNamesChange({ experimentName, model });
+  });
 }
