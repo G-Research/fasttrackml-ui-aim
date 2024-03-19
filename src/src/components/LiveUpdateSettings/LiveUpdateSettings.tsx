715c2f59d4c2bc71a674c042682ca120f25bbb93
Geoff Wilson <geoff@gr-oss.io>
2024-03-19 13:58:42 -0400

    Pickup "live_updates_enabled" field and show/hide accordingly

--- src/src/components/LiveUpdateSettings/LiveUpdateSettings.tsx
+++ src/src/components/LiveUpdateSettings/LiveUpdateSettings.tsx
@@ -8,29 +8,32 @@ import './LiveUpdateSettings.scss';
 export interface ILiveUpdateSettingsProp {
   delay: number;
   enabled: boolean;
-  onLiveUpdateConfigChange: ({ enabled }: { enabled: boolean }) => void;
 }
 
 function LiveUpdateSettings(
   props: ILiveUpdateSettingsProp,
 ): React.FunctionComponentElement<React.ReactNode> {
-  return (
-    <ErrorBoundary>
-      <div className='LiveUpdateSettings'>
-        <Text className='LiveUpdateSettings__Text' size={14}>
-          Live Update:
-        </Text>
-        <Switcher
-          checked={Boolean(props.enabled)}
-          onChange={() => {
-            props.onLiveUpdateConfigChange({ enabled: !props.enabled });
-          }}
-          size='small'
-          color='primary'
-        />
-      </div>
-    </ErrorBoundary>
-  );
+  if (window.live_updates_enabled == 1) {
+    return (
+      <ErrorBoundary>
+        <div className='LiveUpdateSettings'>
+          <Text className='LiveUpdateSettings__Text' size={14}>
+            Live Update:
+          </Text>
+          <Switcher
+            checked={Boolean(props.enabled)}
+            onChange={() => {
+              props.onLiveUpdateConfigChange({ enabled: !props.enabled });
+            }}
+            size='small'
+            color='primary'
+          />
+        </div>
+      </ErrorBoundary>
+    );
+  }
+  props.onLiveUpdateConfigChange({ enabled: false });
+  return null;
 }
 
 export default React.memo<ILiveUpdateSettingsProp>(LiveUpdateSettings);
