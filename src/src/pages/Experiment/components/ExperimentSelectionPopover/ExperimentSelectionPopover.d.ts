98d1f0604d984a1b8d465bb660138937cf837ae4
Juan Escalada <juanescalada175@gmail.com>
2024-04-05 08:05:15 -0700

    Fix experiment header UI refresh issue

--- src/src/pages/Experiment/components/ExperimentSelectionPopover/ExperimentSelectionPopover.d.ts
+++ src/src/pages/Experiment/components/ExperimentSelectionPopover/ExperimentSelectionPopover.d.ts
@@ -2,9 +2,10 @@ import { IExperimentData } from 'modules/core/api/experimentsApi/types';
 
 export interface IExperimentSelectionPopoverProps {
   experimentsData: IExperimentData[] | null;
-  selectedExperimentNames: string[];
+  selectedExperiments: string[];
   isExperimentsLoading: boolean;
   getExperimentsData: () => void;
+  setSelectedExperiments: (selectedExperiments: string[]) => void;
   onSelectExperimentNamesChange: (experimentName: string) => void;
   onToggleAllExperiments: (experimentNames: string[]) => void;
 }
