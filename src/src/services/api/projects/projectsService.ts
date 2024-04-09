9f4cc0c3e51345f889a3b456293f3cf9f91bcc98
fabiovincenzi <93596376+fabiovincenzi@users.noreply.github.com>
2024-04-09 15:33:19 +0200

    Do not call projects/params endpoint if no experiment is selected (#63)

--- src/src/services/api/projects/projectsService.ts
+++ src/src/services/api/projects/projectsService.ts
@@ -26,6 +26,22 @@ function getProjectParams(
   sequences: string[] = ['metric'],
   selectedExperimentNames: string[] = [],
 ): IApiRequest<IProjectParamsMetrics> {
+  if (selectedExperimentNames.length === 0) {
+    const controller = new AbortController();
+    return {
+      call: () =>
+        new Promise((resolve: (data: IProjectParamsMetrics) => any) => {
+          // Simulating an empty response
+          const data: IProjectParamsMetrics = {
+            metric: {},
+            images: {},
+            params: {},
+          };
+          resolve(data);
+        }),
+      abort: () => controller.abort(),
+    };
+  }
   const query =
     sequences.reduce((acc: string, sequence: string, index: number) => {
       acc += `${index === 0 ? '?' : '&'}sequence=${sequence}`;
