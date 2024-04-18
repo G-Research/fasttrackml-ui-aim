0f282c480e1b6aa7d160d76fbeb986235dd09dec
Geoff Wilson <geoff@gr-oss.io>
2024-04-18 15:43:27 -0400

    WIP adding ExperimentBar to RunsBar

--- src/src/pages/Runs/components/RunsBar/RunsBar.tsx
+++ src/src/pages/Runs/components/RunsBar/RunsBar.tsx
@@ -11,6 +11,17 @@ import ControlPopover from 'components/ControlPopover/ControlPopover';
 import pageTitlesEnum from 'config/pageTitles/pageTitles';
 import { DOCUMENTATIONS } from 'config/references';
 
+import { IResourceState } from 'modules/core/utils/createResource';
+import { IExperimentData } from 'modules/core/api/experimentsApi';
+
+import createExperimentEngine from 'pages/Dashboard/components/ExploreSection/ExperimentsCard/ExperimentsStore';
+import ExperimentBar from 'pages/Experiment/components/ExperimentBar';
+import useExperimentState from 'pages/Experiment/useExperimentState';
+
+import { getSelectedExperimentNames } from 'utils/app/getSelectedExperimentNames';
+import onSelectExperimentNamesChange from 'utils/app/onSelectExperimentNamesChange';
+import onToggleAllExperiments from 'utils/app/onToggleAllExperiments';
+
 import 'pages/Metrics/components/MetricsBar/MetricsBar.scss';
 
 function RunsBar(props: {
@@ -18,10 +29,53 @@ function RunsBar(props: {
   delay: number;
   onLiveUpdateConfigChange: () => void;
   disabled: boolean;
+  onSelectExperimentNamesChange: (experimentName: string) => void;
+  onToggleAllExperiments: (experimentNames: string[]) => void;
 }): React.FunctionComponentElement<React.ReactNode> {
+  const { current: experimentsEngine } = React.useRef(createExperimentEngine);
+
+  const experimentsStore: IResourceState<IExperimentData[]> =
+    experimentsEngine.experimentsState((state) => state);
+
+  React.useEffect(() => {
+    experimentsEngine.fetchExperiments();
+    return () => {
+      experimentsEngine.destroy();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const [selectedExperimentNames, setSelectedExperimentNames] = React.useState<
+    string[]
+  >(getSelectedExperimentNames());
+
+  // Fetch all experiments along with default
+  const { experimentState, experimentsState, getExperimentsData } =
+    useExperimentState(experimentsStore.data?.[0]?.id);
+
+  const { data: experimentData, loading: isExperimentLoading } =
+    experimentState;
+
+  const { data: experimentsData, loading: isExperimentsLoading } =
+    experimentsState;
+
+  function handleExperimentNamesChange(experimentName: string): void {
+    onSelectExperimentNamesChange(experimentName);
+    setSelectedExperimentNames(getSelectedExperimentNames());
+  }
+
   return (
     <ErrorBoundary>
       <AppBar title={pageTitlesEnum.RUNS_EXPLORER} disabled={props.disabled}>
+        <ExperimentBar
+          experimentsData={experimentsData}
+          isExperimentLoading={isExperimentLoading}
+          isExperimentsLoading={isExperimentsLoading}
+          selectedExperimentNames={selectedExperimentNames}
+          getExperimentsData={getExperimentsData}
+          onSelectExperimentNamesChange={handleExperimentNamesChange}
+          onToggleAllExperiments={onToggleAllExperiments}
+        />
         <LiveUpdateSettings {...props} />
         <div className='MetricsBar__menu'>
           <ErrorBoundary>
