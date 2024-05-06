fb36be98c377e13c8a997c08b7b6963c055d1e09
fabio vincenzi <fabio.vincenzi2@studio.unibo.it>
2024-05-06 14:14:33 +0200

    make context an object

--- src/src/utils/app/getMetricsListFromSelect.ts
+++ src/src/utils/app/getMetricsListFromSelect.ts
@@ -1,15 +1,12 @@
 import { ISyntaxErrorDetails } from 'types/components/NotificationContainer/NotificationContainer';
 import { ISelectConfig } from 'types/services/models/explorer/createAppModel';
-
-import { jsValidVariableRegex } from 'utils/getObjectPaths';
-
-import { formatValue } from '../formatValue';
+import { IContext } from 'types/services/models/metrics/metricsAppModel';
 
 export default function getMetricsListFromSelect(
   selectData: ISelectConfig,
   error?: ISyntaxErrorDetails,
-): Array<{ key: string; context: string }> {
-  const metricsList: Array<{ key: string; context: string }> = [];
+): Array<{ key: string; context: IContext }> {
+  const metricsList: Array<{ key: string; context: IContext }> = [];
 
   if (selectData === undefined) {
     return metricsList;
@@ -17,10 +14,7 @@ export default function getMetricsListFromSelect(
 
   selectData.options?.forEach((option) => {
     const metricName = option.value?.option_name ?? '';
-    const context: string =
-      option.value?.context && Object.keys(option.value?.context).length > 0
-        ? formatValue(option.value?.context)
-        : '{}';
+    const context: IContext = option.value?.context ?? {};
 
     metricsList.push({ key: metricName, context: context });
   });
