e12915a051d682c5760c1628a67d1a290c88400e
Juan Escalada <juanescalada175@gmail.com>
2024-05-13 12:34:42 -0700

    Add ChartPopoverAdvanced component

--- src/src/pages/Metrics/components/ChartPopover/ChartPopoverAdvanced.tsx
+++ src/src/pages/Metrics/components/ChartPopover/ChartPopoverAdvanced.tsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Checkbox,
-  Divider,
-  TextField,
-} from '@material-ui/core';
+import { Checkbox, TextField } from '@material-ui/core';
 import {
   CheckBox as CheckBoxIcon,
   CheckBoxOutlineBlank,
@@ -15,86 +8,113 @@ import {
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { Button, Box } from '@material-ui/core';
 
-import { Badge, Icon, Text, ToggleButton } from 'components/kit';
+import { Text } from 'components/kit';
 import ErrorBoundary from 'components/ErrorBoundary';
 
-import { GroupNameEnum } from 'config/grouping/GroupingPopovers';
-
-import { IGroupingSelectOption } from 'types/services/models/metrics/metricsAppModel';
+import {
+  IGroupingCondition,
+  IGroupingSelectOption,
+} from 'types/services/models/metrics/metricsAppModel';
 import { IGroupingPopoverAdvancedProps } from 'types/components/GroupingPopover/GroupingPopover';
 
 import './ChartPopoverAdvanced.scss';
 
+export enum IOperator {
+  '==' = '==',
+  '!=' = '!=',
+  '>' = '>',
+  '<' = '<',
+  '>=' = '>=',
+  '<=' = '<=',
+}
+
 function ChartPopoverAdvanced({
-  onPersistenceChange,
-  onGroupingPaletteChange,
-  onShuffleChange,
+  onGroupingConditionsChange,
   groupingData,
   groupingSelectOptions,
-  onSelect,
-  inputLabel,
 }: IGroupingPopoverAdvancedProps): React.FunctionComponentElement<React.ReactNode> {
-  const [conditions, setConditions] = React.useState<string[]>([]);
-
-  function isShuffleDisabled(): boolean {
-    if (groupingData?.chart.length) {
-      return false;
-    }
-    return true;
-  }
-
-  function onAddCondition(condition: string) {
-    setConditions([...conditions, condition]);
-  }
-
+  // Current value for selectedField
   const [inputValue, setInputValue] = React.useState('');
 
-  function onChange(e: any, value: IGroupingSelectOption | null): void {
+  // Controls for the condition inputs
+  const [selectedField, setSelectedField] =
+    React.useState<IGroupingSelectOption | null>(null);
+
+  const [selectedOperator, setSelectedOperator] =
+    React.useState<IOperator | null>(IOperator['==']);
+
+  const [selectedValue, setSelectedValue] = React.useState<string>('');
+
+  // Array of grouping conditions
+  const [conditions, setConditions] = React.useState<IGroupingCondition[]>(
+    groupingData?.conditions || [],
+  );
+
+  const operators = [
+    IOperator['=='],
+    IOperator['!='],
+    IOperator['>'],
+    IOperator['<'],
+    IOperator['>='],
+    IOperator['<='],
+  ];
+
+  function onAddCondition() {
+    // Create a new condition object
+    const condition: IGroupingCondition = {
+      fieldName: selectedField?.label || '',
+      operator: selectedOperator ?? IOperator['=='],
+      value: selectedValue,
+    };
+
+    // If condition fieldName is not in array, add it, else replace
+    const conditionIndex = conditions.findIndex(
+      (c) => c.fieldName === condition.fieldName,
+    );
+    if (conditionIndex === -1) {
+      const newConditions = [...conditions, condition];
+      setConditions(newConditions);
+      onGroupingConditionsChange?.(newConditions);
+    } else {
+      const newConditions = [...conditions];
+      newConditions[conditionIndex] = condition;
+      setConditions(newConditions);
+      onGroupingConditionsChange?.(newConditions);
+    }
+  }
+
+  function onChangeField(e: any, value: IGroupingSelectOption | null): void {
     if (e?.code !== 'Backspace') {
-      handleSelect(value);
+      handleSelectField(value);
     } else {
       if (inputValue.length === 0) {
-        handleSelect(value);
+        handleSelectField(value);
       }
     }
   }
 
-  function handleSelect(value: IGroupingSelectOption | null) {
-    console.log('handleSelect - value: ', value);
-    console.log('handleSelect - onSelect: ', onSelect);
-    // if (onSelect) {
-    //   onSelect({
-    //     groupName: GroupNameEnum.CHART,
-    //     list: values.map((item: IGroupingSelectOption) =>
-    //       typeof item === 'string' ? item : item.value,
-    //     ),
-    //   });
-    // }
+  function onChangeOperator(e: any, value: IOperator): void {
+    handleSelectOperator(value || IOperator['==']);
   }
 
-  const value: IGroupingSelectOption | null = React.useMemo(() => {
-    let data: { value: string; group: string; label: string }[] = [];
+  function handleSelectField(value: IGroupingSelectOption | null) {
+    if (selectedField?.value === value?.value) {
+      setInputValue('');
+      setSelectedField(null);
+      return;
+    }
 
-    // Find and return the user's selected value (unique) from the groupingSelectOptions
-    groupingSelectOptions?.forEach((option) => {
-      if (groupingData?.chart.indexOf(option.value) !== -1) {
-        data.push(option);
-      }
-    });
+    setInputValue(value?.label || '');
+    setSelectedField(value);
+  }
 
-    return data[0] || null;
-  }, [groupingData, groupingSelectOptions]);
+  function handleSelectOperator(value?: IOperator) {
+    setSelectedOperator(value ?? IOperator['==']);
+  }
 
-  //ToDo reverse mode
-  // function handleGroupingMode(val: string | number, id: any) {
-  //   onGroupingModeChange({
-  //     groupName,
-  //     value: val === 'Reverse',
-  //     options: groupingData?.reverseMode[groupName as GroupNameEnum]
-  //       ? groupingSelectOptions
-  //       : null,
-  //   });
-  // }
+  function handleSelectValue(value: string) {
+    setSelectedValue(value);
+  }
 
   const options = React.useMemo(() => {
     if (inputValue.trim() !== '') {
@@ -125,91 +145,125 @@ function ChartPopoverAdvanced({
             size={14}
             className='ChartPopoverAdvanced__conditionalFilter__p'
           >
-            Group charts by conditions such as run.epochs &gt; 30.
+            Group charts by conditions such as{' '}
+            <span style={{ fontFamily: 'Courier New' }}>
+              run.epochs &gt; 30
+            </span>
+            .
           </Text>
-          <div className='flex fac fjb'>
-            <Button
-              onClick={() => onAddCondition('test')}
-              variant='contained'
-              size='medium'
-            >
-              Add Condition
-            </Button>
+          <div className='flex fac'>
             {/* Add textbox to allow grouping by condition */}
-            {/* <AutocompleteInput
-              advanced
-              context={autocompleteContext.advancedSuggestions}
-              value={query.advancedInput}
-              onChange={onInputChange}
-              onEnter={onSubmit}
-              error={processedError}
-              forceRemoveError={true}
-            /> */}
+            <Autocomplete
+              className='ChartPopoverAdvanced__container__select__fieldName'
+              openOnFocus
+              size='small'
+              disableClearable={true}
+              options={options}
+              value={selectedField ?? undefined}
+              onChange={onChangeField}
+              groupBy={(option) => option.group}
+              getOptionLabel={(option) => option.label}
+              getOptionSelected={(option, value) =>
+                option.value === selectedField?.value
+              }
+              renderInput={(params: any) => (
+                <TextField
+                  {...params}
+                  inputProps={{
+                    ...params.inputProps,
+                    value: inputValue,
+                    onChange: (e: any) => {
+                      setInputValue(e.target?.value);
+                    },
+                  }}
+                  className='TextField__OutLined__Small'
+                  variant='outlined'
+                  placeholder='Select fields'
+                />
+              )}
+              renderTags={() => null} // No tags for single selection
+              renderOption={(option, { selected }) => (
+                <div
+                  className='GroupingPopover__option'
+                  onClick={() => onChangeField(null, option)}
+                >
+                  <Checkbox
+                    color='primary'
+                    size='small'
+                    icon={<CheckBoxOutlineBlank />}
+                    checkedIcon={<CheckBoxIcon />}
+                    style={{ marginRight: 4 }}
+                    checked={selected}
+                  />
+                  <Text className='GroupingPopover__option__label' size={14}>
+                    {option.label}
+                  </Text>
+                </div>
+              )}
+            />
+            {/* Dropdown for operator */}
+            <Autocomplete
+              className='ChartPopoverAdvanced__container__select__operator'
+              size='small'
+              openOnFocus
+              disableClearable={true}
+              options={operators}
+              value={selectedOperator ?? IOperator['==']}
+              onChange={onChangeOperator}
+              placeholder='Select operator'
+              renderInput={(params: any) => (
+                <TextField
+                  {...params}
+                  className='TextField__OutLined__Small'
+                  variant='outlined'
+                  placeholder='Select operator'
+                />
+              )}
+            />
+            {/* Textbox for the condition value */}
+            <TextField
+              className='ChartPopoverAdvanced__container__select__value'
+              variant='outlined'
+              size='small'
+              placeholder='Enter value'
+              value={selectedValue}
+              onChange={(e) => handleSelectValue(e.target.value)}
+            />
           </div>
+          <Button
+            className='ChartPopoverAdvanced__container__select__button'
+            onClick={() => onAddCondition()}
+            variant='outlined'
+            size='medium'
+            disabled={!selectedField || !selectedOperator || !selectedValue}
+          >
+            Add Condition
+          </Button>
           <div>
             {conditions.map((condition, index) => (
               <Box
                 key={index}
-                className='ChartPopoverAdvanced__conditionalFilter__box'
+                className='ChartPopoverAdvanced__conditionalFilter__box flex fac fjb'
               >
-                <Autocomplete
-                  openOnFocus
+                {/* Show condition and button in same line */}
+                <Text
+                  size={14}
+                  className='ChartPopoverAdvanced__conditionalFilter__box__p'
+                >
+                  {condition.fieldName} {condition.operator} {condition.value}
+                </Text>
+                <Button
+                  onClick={() => {
+                    const newConditions = [...conditions];
+                    newConditions.splice(index, 1);
+                    setConditions(newConditions);
+                    if (onGroupingConditionsChange) {
+                      onGroupingConditionsChange(newConditions);
+                    }
+                  }}
+                  variant='contained'
                   size='small'
-                  options={options}
-                  value={value}
-                  onChange={onChange}
-                  groupBy={(option) => option.group}
-                  getOptionLabel={(option) => option.label}
-                  getOptionSelected={(option, value) =>
-                    option.value === value.value
-                  }
-                  renderInput={(params: any) => (
-                    <TextField
-                      {...params}
-                      inputProps={{
-                        ...params.inputProps,
-                        value: inputValue,
-                        onChange: (e: any) => {
-                          setInputValue(e.target?.value);
-                        },
-                      }}
-                      className='TextField__OutLined__Small'
-                      variant='outlined'
-                      placeholder='Select fields'
-                    />
-                  )}
-                  renderTags={(value, getTagProps) => (
-                    <div className='GroupingPopover__container__select__selectedFieldsContainer'>
-                      {value.map((selected, i) => (
-                        <Badge
-                          key={i}
-                          {...getTagProps({ index: i })}
-                          label={selected.label}
-                          selectBadge={true}
-                        />
-                      ))}
-                    </div>
-                  )}
-                  renderOption={(option, { selected }) => (
-                    <div className='GroupingPopover__option'>
-                      <Checkbox
-                        color='primary'
-                        size='small'
-                        icon={<CheckBoxOutlineBlank />}
-                        checkedIcon={<CheckBoxIcon />}
-                        style={{ marginRight: 4 }}
-                        checked={selected}
-                      />
-                      <Text
-                        className='GroupingPopover__option__label'
-                        size={14}
-                      >
-                        {option.label}
-                      </Text>
-                    </div>
-                  )}
-                />
-                <Button variant='contained' size='small'>
+                >
                   Remove
                 </Button>
               </Box>
