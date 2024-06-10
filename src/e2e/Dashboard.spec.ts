00890b866060b34e7fa74ef0c3c16bebe1a78b3f
Juan Escalada <juanescalada175@gmail.com>
2024-06-10 09:26:35 -0700

    Add rudimentary test for Dashboard

---
+++ src/e2e/Dashboard.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+const BASE_URL = 'http://localhost:3000';
+
+test.describe('Dashboard', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
+  });
+
+  test('has title', async ({ page }) => {
+    await expect(page).toHaveTitle('FastTrackML (modern)');
+  });
+
+  test('active runs link redirects correctly', async ({ page }) => {
+    await page.getByText('Active Runs').click({ force: true });
+
+    // Check if there is a textbox with the value 'runs.active == True' (do not use toHaveSelector or toHaveText)
+    await page.getByRole('code', { name: 'runs.active == True' });
+  });
+});
