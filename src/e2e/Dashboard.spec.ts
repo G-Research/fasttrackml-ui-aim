3896726a9c57c9d8f612f5060e885018fa54073c
Juan Escalada <juanescalada175@gmail.com>
2024-06-10 12:37:48 -0700

    Add tests for dashboard quick navigation buttons

--- src/e2e/Dashboard.spec.ts
+++ src/e2e/Dashboard.spec.ts
@@ -14,7 +14,22 @@ test.describe('Dashboard', () => {
   test('active runs link redirects correctly', async ({ page }) => {
     await page.getByText('Active Runs').click({ force: true });
 
-    // Check if there is a textbox with the value 'runs.active == True' (do not use toHaveSelector or toHaveText)
     await page.getByRole('code', { name: 'runs.active == True' });
+  });
+
+  test('archived runs link redirects correctly', async ({ page }) => {
+    await page.getByText('Archived Runs').click({ force: true });
+
+    await page.getByRole('code', { name: 'runs.active == False' });
+  });
+
+  test("last week's runs link redirects correctly", async ({ page }) => {
+    await page.getByText("Last Week's Runs").click({ force: true });
+
+    // The text varies depending on the current date:
+    // Example: datetime(2024, 6, 3) <= run.created_at < datetime(2024, 6, 10)
+    await page.getByRole('code', {
+      name: /datetime\(\d+, \d+, \d+\) <= run\.created_at < datetime\(\d+, \d+, \d+\)/,
+    });
   });
 });
