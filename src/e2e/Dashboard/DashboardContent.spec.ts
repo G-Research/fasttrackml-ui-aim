0875d4f98af287fe27b53c1a3feaa4459f83e63c
Juan Escalada <juanescalada175@gmail.com>
2024-06-19 09:21:32 -0700

    Fix DashboardContent test

--- src/e2e/Dashboard/DashboardContent.spec.ts
+++ src/e2e/Dashboard/DashboardContent.spec.ts
@@ -7,6 +7,7 @@ test.describe('Dashboard', () => {
     page.on('console', (msg) => {
       console.log('PAGE LOG:', msg.text());
     });
+
     await page.goto(BASE_URL);
     await page.screenshot({ path: 'before-waitForLoadState.png' });
     await page.waitForLoadState('networkidle');
@@ -15,43 +16,11 @@ test.describe('Dashboard', () => {
 
   // Test that the total number of runs is two. This assumes that the
   // test database has been seeded with two runs by running `k6 run k6_load.js`
-
   test('has two runs', async ({ page }) => {
-    const debugScreenshotPath = 'debug-screenshot.png';
-    const debugHtmlPath = 'debug-content.html';
-
-    // Capture initial screenshot and HTML to see initial state
-    await page.screenshot({ path: debugScreenshotPath });
-
-    // Wait for the Spinner to appear
-    await page.waitForSelector('.Spinner', {
-      state: 'visible',
-      timeout: 10000,
-    });
-
-    // Capture screenshot and HTML while loading
-    await page.screenshot({
-      path: debugScreenshotPath.replace('.png', '-loading.png'),
-    });
-
-    // Wait for the Spinner to disappear
-    await page.waitForSelector('.Spinner', { state: 'hidden', timeout: 10000 });
-
-    // Capture screenshot and HTML after loading
-    await page.screenshot({
-      path: debugScreenshotPath.replace('.png', '-loaded.png'),
-    });
-
-    // Wait for the ProjectStatistics component to appear
-    await page.waitForSelector('p.ProjectStatistics__totalRuns', {
-      timeout: 10000,
-    });
-
     // Check for the total runs element
     const textContent = await page.textContent(
       'p.ProjectStatistics__totalRuns',
     );
-    console.log('Text content:', textContent);
     expect(textContent).toBe('Total runs: 2');
   });
 });
