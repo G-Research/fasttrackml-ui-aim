b906132dffe9f3ff686b673811fc0a85840d15f9
Juan Escalada <juanescalada175@gmail.com>
2024-06-20 14:39:44 -0700

    Add tests for experiments table

--- src/e2e/Dashboard/DashboardContent.spec.ts
+++ src/e2e/Dashboard/DashboardContent.spec.ts
@@ -16,4 +16,41 @@ test.describe('Dashboard', () => {
     );
     expect(textContent).toBe('Total runs: 2');
   });
+
+  test('has two experiments', async ({ page }) => {
+    const textContent = await page.textContent('h3.ExperimentsCard__title');
+    expect(textContent).toBe('Experiments (2)');
+  });
+
+  test('Default experiment exists and has no runs', async ({ page }) => {
+    const table = await page.$('.BaseTable__body');
+    const rows = await table?.$$('.BaseTable__row');
+    expect(rows).not.toBeNull();
+
+    const firstRow = rows?.[0];
+    const experimentName = await firstRow?.$eval(
+      '.ExperimentNameBox__experimentName a',
+      (el) => el.textContent,
+    );
+    expect(experimentName).toBe('Default');
+
+    const runCount = await firstRow?.$eval('p', (el) => el.textContent);
+    expect(runCount).toBe('0');
+  });
+
+  test('generated experiment exists and has two runs', async ({ page }) => {
+    const table = await page.$('.BaseTable__body');
+    const rows = await table?.$$('.BaseTable__row');
+    expect(rows).not.toBeNull();
+
+    const secondRow = rows?.[1];
+    const experimentName = await secondRow?.$eval(
+      '.ExperimentNameBox__experimentName a',
+      (el) => el.textContent,
+    );
+    expect(experimentName).toContain('experiment-');
+
+    const runCount = await secondRow?.$eval('p', (el) => el.textContent);
+    expect(runCount).toBe('2');
+  });
 });
