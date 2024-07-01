c94c096e75017a07daf7bf07562e3131587a6b0f
Juan Escalada <juanescalada175@gmail.com>
2024-07-01 14:06:56 -0700

    Test navigating from dashboard to runs

--- src/e2e/RunsExplorer/RunsExplorer.spec.ts
+++ src/e2e/RunsExplorer/RunsExplorer.spec.ts
@@ -1,11 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-const BASE_URL = 'http://localhost:3000/aim/runs';
+const BASE_URL = 'http://localhost:3000/aim';
+const RUNS_PAGE = `${BASE_URL}/runs`;
 
 test.describe('Runs Explorer', () => {
   test.beforeEach(async ({ page }) => {
+    await page.goto(RUNS_PAGE);
+    await page.waitForLoadState('networkidle');
+  });
+
+  test('navigates correctly from dashboard', async ({ page }) => {
     await page.goto(BASE_URL);
     await page.waitForLoadState('networkidle');
+    await page.click('text=Runs');
+    await page.waitForLoadState('networkidle');
+    expect(page.url()).toBe(RUNS_PAGE);
   });
 
   test('displays no results message when no experiments are selected', async ({
@@ -16,31 +25,6 @@ test.describe('Runs Explorer', () => {
   });
 
   test('has two runs if generated experiment is selected', async ({ page }) => {
-    // Add the generated experiment to the selection and wait for the table to appear
-    await page.click(
-      '.ExperimentBar__headerContainer__appBarTitleBox__buttonSelectToggler',
-    );
-
-    const button = page.locator('button', { hasText: /^experiment-/ });
-    await button.click();
-
-    await page.waitForSelector('.Table__cell.undefined.experiment');
-
-    const rows = page.locator(
-      '.Table__cell.undefined.experiment .ExperimentNameBox__experimentName',
-    );
-
-    expect(await rows.count()).toBe(2);
-
-    for (let i = 0; i < 2; i++) {
-      const runName = await rows.nth(i).innerText();
-      expect(runName).toMatch(/^experiment-/);
-    }
-  });
-
-  test('has two runs if generated experiment is selected repeat', async ({
-    page,
-  }) => {
     // Add the generated experiment to the selection and wait for the table to appear
     await page.click(
       '.ExperimentBar__headerContainer__appBarTitleBox__buttonSelectToggler',
