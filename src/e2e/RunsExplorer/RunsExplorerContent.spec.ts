98948670277324a9c805cfca9e8847a992eb4bdd
Juan Escalada <juanescalada175@gmail.com>
2024-07-01 11:49:10 -0700

    Add test for selected generated experiment

--- src/e2e/RunsExplorer/RunsExplorerContent.spec.ts
+++ src/e2e/RunsExplorer/RunsExplorerContent.spec.ts
@@ -8,4 +8,33 @@ test.describe('Runs Explorer', () => {
     await page.waitForLoadState('networkidle');
   });
 
+  test('displays no results message when no experiments are selected', async ({
+    page,
+  }) => {
+    const runsTable = await page.locator('.RunsTable');
+    expect(await runsTable.locator('text=No results').count()).toBe(1);
+  });
+
+  test('has two runs if generated experiment is selected', async ({ page }) => {
+    // Add the generated experiment to the selection and wait for the table to appear
+    await page.click(
+      '.ExperimentBar__headerContainer__appBarTitleBox__buttonSelectToggler',
+    );
+
+    const button = page.locator('button', { hasText: /^experiment-/ });
+    await button.click();
+
+    await page.waitForSelector('.Table__cell.undefined.experiment');
+
+    const rows = page.locator(
+      '.Table__cell.undefined.experiment .ExperimentNameBox__experimentName',
+    );
+
+    expect(await rows.count()).toBe(2);
+
+    for (let i = 0; i < 2; i++) {
+      const runName = await rows.nth(i).innerText();
+      expect(runName).toMatch(/^experiment-/);
+    }
+  });
 });
