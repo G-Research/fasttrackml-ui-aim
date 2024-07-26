fdf03c11dd69371571374b4e69f5f25c5086053f
Juan Escalada <juanescalada175@gmail.com>
2024-07-26 09:04:17 -0700

    Add index/count based fetching for useRunMetricsBatch

--- src/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
+++ src/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 import runDetailAppModel from 'services/models/runs/runDetailAppModel';
 
-function useRunMetricsBatch({ runTraces, runHash }: any) {
+function useRunMetricsBatch({
+  runTraces,
+  runHash,
+  startIndex,
+  count,
+  fetchMore,
+}: {
+  runTraces: any;
+  runHash: string;
+  startIndex?: number;
+  count?: number;
+  fetchMore?: () => void;
+}) {
   React.useEffect(() => {
+    if (startIndex !== undefined && count) {
+      console.log(
+        'sliced metrics: ',
+        runTraces.metric.slice(startIndex, startIndex + count),
+      );
+    }
     const runsBatchRequestRef = runDetailAppModel.getRunMetricsBatch(
-      runTraces.metric,
+      startIndex !== undefined && count
+        ? runTraces.metric.slice(startIndex, startIndex + count)
+        : runTraces.metric,
       runHash,
     );
 
@@ -15,7 +35,13 @@ function useRunMetricsBatch({ runTraces, runHash }: any) {
       runsBatchRequestRef.abort();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [runTraces, runHash]);
+  }, [runTraces, runHash, startIndex, count]);
+
+  React.useEffect(() => {
+    if (fetchMore) {
+      fetchMore();
+    }
+  }, [fetchMore]);
 }
 
 export default useRunMetricsBatch;
