e87591a6afa4f8e8231e0dfddbba9d5af06e8e74
Juan Escalada <juanescalada175@gmail.com>
2024-07-30 10:32:50 -0700

    Fix and clean up useRunMetricsBatch hook

--- src/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
+++ src/src/pages/RunDetail/hooks/useRunMetricsBatch.tsx
@@ -5,43 +5,32 @@ import runDetailAppModel from 'services/models/runs/runDetailAppModel';
 function useRunMetricsBatch({
   runTraces,
   runHash,
-  startIndex,
-  count,
-  fetchMore,
+  startIndex = 0,
+  count = 500, // Error occurs if count is greater than 1000
 }: {
   runTraces: any;
   runHash: string;
   startIndex?: number;
   count?: number;
-  fetchMore?: () => void;
 }) {
   React.useEffect(() => {
-    if (startIndex !== undefined && count) {
-      console.log(
-        'sliced metrics: ',
-        runTraces.metric.slice(startIndex, startIndex + count),
+    const fetchMetricsBatch = () => {
+      const runsBatchRequestRef = runDetailAppModel.getRunMetricsBatch(
+        startIndex !== undefined && count
+          ? runTraces.metric.slice(startIndex, startIndex + count)
+          : runTraces.metric,
+        runHash,
       );
-    }
-    const runsBatchRequestRef = runDetailAppModel.getRunMetricsBatch(
-      startIndex !== undefined && count
-        ? runTraces.metric.slice(startIndex, startIndex + count)
-        : runTraces.metric,
-      runHash,
-    );
 
-    runsBatchRequestRef.call();
+      runsBatchRequestRef.call();
 
-    return () => {
-      runsBatchRequestRef.abort();
+      return () => {
+        runsBatchRequestRef.abort();
+      };
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [runTraces, runHash, startIndex, count]);
 
-  React.useEffect(() => {
-    if (fetchMore) {
-      fetchMore();
-    }
-  }, [fetchMore]);
+    fetchMetricsBatch();
+  }, [runTraces, runHash, startIndex, count]);
 }
 
 export default useRunMetricsBatch;
