01bd5a6f6b7701d893cdb2ec19ce1bb84a3c7c68
Juan Escalada <juanescalada175@gmail.com>
2024-09-04 12:18:06 +0900

    Add test for checking image caption

--- src/e2e/ImagesExplorer/ImagesExplorer.spec.ts
+++ src/e2e/ImagesExplorer/ImagesExplorer.spec.ts
@@ -17,6 +17,35 @@ test.describe('Images Explorer', () => {
     expect(page.url()).toBe(IMAGES_PAGE);
   });
 
+  test('Check if image caption is present', async ({ page }) => {
+    // Select the latest experiment
+    const experimentsIcon = page.locator('.icon-arrow-down');
+    await experimentsIcon.click();
+
+    const experiment = page.locator(
+      '.ExperimentSelectionPopover__contentContainer__experimentsListContainer__experimentList > :last-child',
+    );
+    await experiment.click();
+
+    // Close the experiment selection popover
+    await page.mouse.click(0, 0);
+
+    // Select the available image name and search
+    const imagesIcon = page.locator('.icon-plus');
+    await imagesIcon.click();
+    const option = page.locator('.SelectForm__option');
+    await option.click();
+    const searchIcon = page.locator('.icon-search');
+    await searchIcon.click();
+
+    await page.waitForSelector('.Table');
+    const mediaSet = page.locator('.MediaSet');
+    const imageBoxWithCaption = mediaSet.locator('.ImageBox .Text', {
+      hasText: 'example image caption',
+    });
+
+    expect(await imageBoxWithCaption.count()).toBeGreaterThan(0);
+  });
 
   test.afterEach(async ({ page }, testInfo) => {
     if (testInfo.status !== testInfo.expectedStatus) {
